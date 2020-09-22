3d2d42ff4847940f9f184936f92bc9354f298e5d
Martin Lopardo <tincho921007cs@gmail.com>
2020-09-22 20:12:10 -0300

    Cree model, traigo todos los datos de ambas tablas

---
+++ model/movieModel.php
@@ -0,0 +1,35 @@
+<?php 
+
+    class model{
+
+        private $db;
+
+        function __constructor(){
+            $this->db = new PDO('mysql:host=localhost;'.'dbname=db_cinema;charset=utf8', 'root', '');
+        }
+
+        //traigo todos los elementos de la tabla pelicula
+        function getMovies(){
+            $query = $this->db->prepare('SELECT * FROM pelicula');
+            $query->execute();
+            $movies = $query->fetchAll(PDO::FETCH_OBJ);
+            return $movies;
+        }
+
+        //traigo todos los elementos de la tabla sala
+        function getRoom(){
+            $query = $this->db->prepare('SELECT * FROM sala');
+            $query->execute();
+            $room = $query->fetchAll(PDO::FETCH_OBJ);
+            return $room;
+        }
+
+    }
+
+
+
+
+
+
+
+?>
