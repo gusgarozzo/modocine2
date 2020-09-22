1a949bc5acdd5b586818883fe810b2507f4efc60
Gustavo Garozzo <56453950+gusgarozzo@users.noreply.github.com>
2020-09-22 14:33:30 -0300

    add: carpetas mvc + routerclass

---
+++ router.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'RouterClass.php';
+    
+    // CONSTANTES PARA RUTEO
+    define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+
+    $r = new Router();
+
+    
