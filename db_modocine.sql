86260ac5142736f0ed15c854c5e5274c58c059fc
Gustavo Garozzo <56453950+gusgarozzo@users.noreply.github.com>
2020-09-25 20:32:25 -0300

    bbdd actualizada

--- db_modocine.sql
+++ db_modocine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-09-2020 a las 17:38:54
+-- Tiempo de generación: 26-09-2020 a las 01:31:13
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -31,6 +31,8 @@ CREATE TABLE `pelicula` (
   `id` int(11) NOT NULL,
   `nombre` varchar(40) NOT NULL,
   `genero` varchar(40) NOT NULL,
+  `sinopsis` text CHARACTER SET latin1 NOT NULL,
+  `puntaje_imdb` float NOT NULL,
   `id_sala` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -38,13 +40,13 @@ CREATE TABLE `pelicula` (
 -- Volcado de datos para la tabla `pelicula`
 --
 
-INSERT INTO `pelicula` (`id`, `nombre`, `genero`, `id_sala`) VALUES
-(1, 'The Batman', 'Accion', 1),
-(2, 'Tenet', 'Suspenso', 2),
-(3, 'Wonder Woman 1984', 'Aventura', 3),
-(4, 'Ford vs Ferrari', 'Drama', 1),
-(5, 'Annabelle 3', 'Terror', 2),
-(6, 'Joker', 'Drama', 3);
+INSERT INTO `pelicula` (`id`, `nombre`, `genero`, `sinopsis`, `puntaje_imdb`, `id_sala`) VALUES
+(1, 'La Monja', 'Terror', 'Una monja se suicida en una abadía rumana y el Vaticano envía a un sacerdote y una novicia a investigar lo sucedido. Lo que ambos encuentran allá es un secreto perverso que los enfrentará cara a cara con el mal en su esencia más pura.', 5.3, 1),
+(2, 'Tenet', 'Suspenso', 'Una acción épica que gira en torno al espionaje internacional, los viajes en el tiempo y la evolución, en la que un agente secreto debe prevenir la Tercera Guerra Mundial.', 7.8, 2),
+(3, 'Wonder Woman 1984', 'Aventura', 'Diana Prince, conocida como Wonder Woman se enfrenta a Cheetah, una villana que posee fuerza y agilidad sobrehumanas.', 7.4, 3),
+(4, 'Ford vs Ferrari', 'Drama', 'El visionario automovilístico Carroll Shelby y su conductor británico Ken Miles reciben la misión de construir un nuevo automóvil con el fin de derrocar el dominio de Ferrari en el Campeonato del Mundo de Le Mans de 1966.', 8.1, 1),
+(5, 'John Wick: Chapter 3 – Parabellum', 'Accion', 'John Wick se da a la fuga, pero el trabajo se le dificulta ya que es el objetivo de todos los asesinos a sueldo del mundo luego de que saliera una recompensa de 14 millones de dólares por su cabeza.', 7.5, 2),
+(6, 'Joker', 'Drama', 'Arthur Fleck adora hacer reír a la gente, pero su carrera como comediante es un fracaso. El repudio social, la marginación y una serie de trágicos acontecimientos lo conducen por el sendero de la locura y, finalmente, cae en el mundo del crimen.', 8.5, 3);
 
 -- --------------------------------------------------------
 
@@ -54,17 +56,19 @@ INSERT INTO `pelicula` (`id`, `nombre`, `genero`, `id_sala`) VALUES
 
 CREATE TABLE `sala` (
   `id` int(11) NOT NULL,
-  `numero` int(11) NOT NULL
+  `letra` varchar(11) CHARACTER SET latin1 NOT NULL,
+  `capacidad` int(11) NOT NULL,
+  `formato` varchar(10) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `sala`
 --
 
-INSERT INTO `sala` (`id`, `numero`) VALUES
-(1, 1),
-(2, 2),
-(3, 3);
+INSERT INTO `sala` (`id`, `letra`, `capacidad`, `formato`) VALUES
+(1, 'A', 120, '2D'),
+(2, 'B', 100, '3D'),
+(3, 'C', 150, '2D');
 
 --
 -- Índices para tablas volcadas
