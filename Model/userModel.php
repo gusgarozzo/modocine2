ae44fa5fa96f84a79bdd933b8f918674a07183f2
Martin Lopardo <tincho921007cs@gmail.com>
2020-11-11 18:47:10 -0300

    Agregue funciones para traer usuario por id, editar usuario, traer los datos de todos los usuarios

--- Model/userModel.php
+++ Model/userModel.php
@@ -10,7 +10,14 @@
             $this->db = new PDO('mysql:host=localhost;'.'dbname=modocine;charset=utf8', 'root', '');
         }
 
-        function getUser($user){
+        function getAllUsers(){
+            $query=$this->db->prepare('SELECT * FROM usuario');
+            $query->execute();
+            $allUsers = $query->fetchAll(PDO::FETCH_OBJ);
+            return $allUsers;  
+        }
+
+        function getUserInfo($user){
             $query = $this->db->prepare('SELECT * FROM usuario WHERE email=?');
             $query->execute(array($user));
             $datos=$query->fetch(PDO::FETCH_OBJ);
@@ -20,6 +27,18 @@
         function saveUserInDDBB($username, $hash){
             $query = $this->db->prepare("INSERT INTO usuario(email, password) VALUES(?,?)");
             $query->execute(array($username, $hash));
+        }
+
+        function getUserById($user_id){
+            $query = $this->db->prepare('SELECT usuario.id, usuario.email, usuario.admin FROM usuario WHERE id=?');
+            $query->execute(array($user_id));
+            $data = $query->fetchAll(PDO::FETCH_OBJ);
+            return $data;
+        }
+
+        function editPermission($user_id, $isAdmin){
+            $query = $this->db->prepare('UPDATE usuario SET admin=? WHERE id=?');
+            $query->execute(array($isAdmin, $user_id));
         }
     
     }
