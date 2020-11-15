b0767b92d5dea89b214822ded16f8b280ca41a5b
Martin Lopardo <tincho921007cs@gmail.com>
2020-11-15 19:50:01 -0300

    Agrego funcion getComments

--- Model/commentModel.php
+++ Model/commentModel.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './api/ApiMovieController.php';
+require_once './api/ApiComentController.php';
 
 class CommentModel{
     private $db;
@@ -9,18 +9,25 @@ class CommentModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=modocine;charset=utf8', 'root', '');
     }
 
+    function getComments(){
+        $query=$this->db->prepare('SELECT * FROM comentario');
+        $query->execute();
+        $comentarios = $query->fetchAll(PDO::FETCH_OBJ);
+        return $comentarios;
+    }
+
     function addCommentModel($usuario, $pelicula_id, $puntaje, $mensaje){
         $query = $this->db->prepare("INSERT INTO comentario(id_usuario, id_pelicula, puntaje, comentario) VALUES(?,?,?,?)");
         $query->execute(array($usuario, $pelicula_id, $puntaje, $mensaje));
         return $query->rowCount();
     }
 
-    function getMensaje($id){
+    function getComment($id){
         $query = $this->db->prepare('SELECT comentario.id, comentario.id_usuario, comentario.id_pelicula, comentario.puntaje, 
             comentario.comentario, usuario.id, usuario.email, pelicula.id, pelicula.nombre FROM comentario INNER JOIN usuario 
                 ON comentario.id_usuario = usuario.id INNER JOIN pelicula ON pelicula.id =comentario.id_pelicula WHERE comentario.id=?');
         $query->execute(array($id));
-        $post = $query->fetchAll(PDO::FETCH_OBJ);
-        return $post;
+        $comment = $query->fetchAll(PDO::FETCH_OBJ);
+        return $comment;
     }
 }
