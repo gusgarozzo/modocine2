4f0cc39adf55828bdcea1abb82cdfd5c50632231
Martin Lopardo <tincho921007cs@gmail.com>
2020-11-25 19:13:29 -0300

    Manejo de error de session_start

--- helpers/authHelper.php
+++ helpers/authHelper.php
@@ -5,18 +5,20 @@ class AuthHelper {
     public function __construct() {}
 
     public function sessionController(){
-        session_start(); 
-        if (!isset($_SESSION['usuario'])){
-            header("Location: ".BASE_URL."login");
-            die();
-        }else{
-            // Pasados los 2 minutos de inactividad, se cierra automaticamente la sesión
-            if(isset($_SESSION['timeout']) && (time()-$_SESSION['timeout'] > 800)){
-                header("Location: ".BASE_URL."logout");  
+        if(!isset($_SESSION)){ 
+            session_start(); 
+            if (!isset($_SESSION['usuario'])){
+                header("Location: ".BASE_URL."login");
                 die();
+            }else{
+                // Pasados los 2 minutos de inactividad, se cierra automaticamente la sesión
+                if(isset($_SESSION['timeout']) && (time()-$_SESSION['timeout'] > 800)){
+                    header("Location: ".BASE_URL."logout");  
+                    die();
+                }
+                // De lo contrario, se actualiza el temporizador
+                $_SESSION['timeout'] = time();
             }
-            // De lo contrario, se actualiza el temporizador
-            $_SESSION['timeout'] = time();
         }
     }
 
