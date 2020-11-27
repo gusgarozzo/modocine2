914538cdaf47f4669dcb54896aa98b948d7da48d
Martin Lopardo <tincho921007cs@gmail.com>
2020-11-27 17:12:24 -0300

    Agrego deleteImg en el model de imagen

--- Model/photoModel.php
+++ Model/photoModel.php
@@ -11,5 +11,10 @@
         function insertPhoto($imagen, $nombre, $tipo, $id_pelicula){
                 $query = $this->db->prepare("INSERT INTO imagen(imagen, nombre, tipo, id_pelicula) VALUES(?,?,?,?)");
                 $query->execute(array($imagen, $nombre, $tipo, $id_pelicula));
-            }
         }
+
+        function deleteImg($img_id){
+            $query = $this->db->prepare("DELETE FROM imagen WHERE id=?");
+            $query->execute(array($img_id));
+        }
+    }
