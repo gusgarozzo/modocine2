8fe5a1a26b287bc4cf51173ae0084bd9eb037891
Martin Lopardo <tincho921007cs@gmail.com>
2020-11-27 18:36:34 -0300

    Agrego getMoviePhotos

--- Model/photoModel.php
+++ Model/photoModel.php
@@ -8,6 +8,13 @@
             $this->db = new PDO('mysql:host=localhost;'.'dbname=modocine;charset=utf8', 'root', '');
         }
 
+        function getMoviePhotos(){
+            $query=$this->db->prepare('SELECT * FROM imagen INNER JOIN pelicula WHERE imagen.id_pelicula = pelicula.id ');
+            $query->execute();
+            $moviePhoto = $query->fetchAll(PDO::FETCH_OBJ);
+            return $moviePhoto;
+        }
+
         function insertPhoto($imagen, $nombre, $tipo, $id_pelicula){
                 $query = $this->db->prepare("INSERT INTO imagen(imagen, nombre, tipo, id_pelicula) VALUES(?,?,?,?)");
                 $query->execute(array($imagen, $nombre, $tipo, $id_pelicula));
